25d28a7039dcbcb8de9a0ae5f1ba308805c61c96
subhajit <karmakarsubhajit123@gmail.com>
2022-08-30 23:17:53 +0530

    Commit#1 FizzBuzz Feature 1_First Test Case is successful

---
+++ src/com/agilekatas/fizzbuzzclasses/FizzBuzz_Features.java
@@ -0,0 +1,8 @@
+package com.agilekatas.fizzbuzzclasses;
+
+public class FizzBuzz_Features {
+
+	public String getfizzbuzzresult(int number) {
+		return String.valueOf(number);
+	}
+}
---
+++ src/com/agilekatas/fizzbuzztests/FizzBuzz_FeaturesTest.java
@@ -0,0 +1,34 @@
+package com.agilekatas.fizzbuzztests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.agilekatas.fizzbuzzclasses.FizzBuzz_Features;
+
+public class FizzBuzz_FeaturesTest {
+
+	private FizzBuzz_Features fizzbuzzfeature;
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Before
+	public void setup() {
+		fizzbuzzfeature = new FizzBuzz_Features();
+	}
+
+	@Test
+	public void test_Feature_1_NormalNumbersReturnSameNumber() {
+		int[] numbers = { 1, 2, 4 };
+		for (int i = 0; i < numbers.length; i++) {
+			String actualresult = fizzbuzzfeature.getfizzbuzzresult(numbers[i]);
+			String expectedresult = String.valueOf(numbers[i]);
+			assertEquals(expectedresult, actualresult);
+		}
+	}
+
+}
